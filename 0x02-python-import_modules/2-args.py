c82890d517f0a96f6ddbfa204bc58a42c7a9d48b
juanstm200 <juans.tovar70@gmail.com>
2021-04-28 21:09:21 -0500

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import sys
 if __name__ == '__main__':
-  lent = len(sys.argv)
-  if lent > 1:
-    print("{:d} arguments:".format((lent - 1)))
-  elif lent == 2:
-    print("{:d} argument:".format((lent - 1)))
-  elif lent == 1:
-    print("{:d} arguments.".format((lent - 1)))
-  for i in range (1, lent):
-    print("{:d}: {}".format(i, sys.argv[i]))
+    from sys import argv
+    lent = len(argv)
+    if lent > 1:
+        print("{:d} arguments:".format((lent - 1)))
+    elif lent == 2:
+        print("{:d} argument:".format((lent - 1)))
+    elif lent == 1:
+        print("{:d} arguments.".format((lent - 1)))
+    for i in range (1, lent):
+        print("{:d}: {}".format(i, argv[i]))
