c265b04fbca4c63968fd83f782ee1a2cbc0c4a54
juanstm200 <juans.tovar70@gmail.com>
2021-05-03 20:46:04 -0500

    calculadora2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-from sys import argv
+    from sys import argv
     from calculator_1 import add, sub, mul, div
     lent = len(argv)
     operador = ['+', '-', '*', '/']
