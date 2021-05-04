838c739798bdfc63447e043df357b38277711ef1
juanstm200 <juans.tovar70@gmail.com>
2021-05-03 20:44:48 -0500

    calculadora

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    from sys import argv
+from sys import argv
     from calculator_1 import add, sub, mul, div
     lent = len(argv)
     operador = ['+', '-', '*', '/']
     if lent != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-  
+
     a = int(argv[1])
     b = int(argv[3])
 
@@ -20,5 +20,5 @@ if __name__ == '__main__':
     elif argv[2] == operador[3]:
         print("{} {} {} = {}".format(a, argv[2], b, div(a, b)))
     else:
-        print("Unknown operator. Available operators: +, -, * and /")      
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
