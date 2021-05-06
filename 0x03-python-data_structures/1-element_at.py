66893043a751394709538e087a0d9601c6412928
juanstm200 <juans.tovar70@gmail.com>
2021-05-05 22:33:24 -0500

    at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     lent = len(my_list)
-    if (idx < 0 or idx != lent):
+    if (idx < 0 or idx >= lent):
         return None
     return(my_list[idx])
