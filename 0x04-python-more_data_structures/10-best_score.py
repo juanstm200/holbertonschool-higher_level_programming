511bdf535eb47166f32bf39822c670efe14a2284
juanstm200 <juans.tovar70@gmail.com>
2021-05-06 22:47:16 -0500

    best

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary and a_dictionary.get:
+        valor = sorted(a_dictionary, reverse=True)
+        return (valor[0])
+    return None
