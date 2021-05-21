a21780b9a604f7e468147bc2f2dafc3914de1c32
juanstm200 <juans.tovar70@gmail.com>
2021-05-20 20:28:23 -0500

    rectangle_width_height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Create class for rectangle."""
+
+
+class Rectangle:
+    """Create constructor for with and height."""
+    def __init__(self, width=0, height=0):
+        """class defenicion"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Proprty to retrieve it"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """property setter to set it"""
+        t_value = type(value)
+        if t_value != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """property height to retrieve it"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """property setter height to retrieve it"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
