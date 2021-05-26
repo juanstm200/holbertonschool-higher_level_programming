5a638806c3fe66fa3a7e3294f2cd9ae3bc6c6108
juanstm200 <juans.tovar70@gmail.com>
2021-05-25 20:32:44 -0500

    test_driven

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Function what sum two integers
+    a convert to integer
+    b convert to integer
+ 
+"""
+
+
+def add_integer(a, b=98):
+    """Returns an integer: the addition of a and b
+        if a or b not is int or float raise,
+        TypeError
+    """
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
