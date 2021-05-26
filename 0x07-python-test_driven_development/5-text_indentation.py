619617034df16122ba1d306f989190426ea6a053
juanstm200 <juans.tovar70@gmail.com>
2021-05-25 23:56:23 -0500

    text_indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" function that prints a text with 2 new
+    lines after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    characters = ['.', '?', ':']
+    for i in characters:
+        text = (i + "\n\n").join(line.strip(" ") for line in text.split(i))
+    print("{}".format(text), end="")
