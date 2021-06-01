857dca9522aa2288a186b5adc4b05f9dcf98d092
juanstm200 <juans.tovar70@gmail.com>
2021-05-31 20:43:42 -0500

    fileWrite

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""function that writes a string to a text file"""
+
+
 def write_file(filename="", text=""):
-    with open(filename, 'w') as textWrite:
+    """Create file and wirte in the file"""
+    with open(filename, mode='w+', encoding="utf-8") as textWrite:
         numChar = textWrite.write(text)
-        return numChar
+    return numChar
