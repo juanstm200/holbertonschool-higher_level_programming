1f3fcdb84284702123f31b4c56bc2e0ae5b2e6fc
juanstm200 <juans.tovar70@gmail.com>
2021-05-31 22:50:48 -0500

    studentFronJson

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""class Student that defines a student"""
+
+
+class Student:
+    """Creatiing the public atributes"""
+    first_name = ""
+    last_name = ""
+    age = ""
+
+    """attributes are initialized"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """Returning a dictionary as and atribute"""
+    def to_json(self, attrs=None):
+        attList = dict()
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            for values in attrs:
+                if values in self.__dict__:
+                    attList.update({values: self.__dict__[values]})
+            return attList
+        return self.__dict__
+
+    """Method that rebuilds a Student's attrs"""
+    def reload_from_json(self, json):
+        for student in json:
+            self.__dict__.update({student: json[student]})
