ebb4e70231ea04655d13b0cf15ef4deadaf70331
juanstm200 <juans.tovar70@gmail.com>
2021-05-31 20:24:56 -0500

    readFile

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-    with open("my_file_0.txt") as f:
-        for line in f:
-            print(line, end="")
+    with open(filename, encoding="utf-8") as f:
+        readText = f.read()
+        print(readText, end="")
