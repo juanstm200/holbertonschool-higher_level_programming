a4e8ace9f1328122677726f1089430b8be266b3a
juanstm200 <juans.tovar70@gmail.com>
2021-06-02 22:23:00 -0500

    Square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class Square that inherits from Rectangle (9-rectangle.py)"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Function that validates the data height and width are of type integer"""
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        super().__init__(size, size)
+        self.__size = size
+
+    """returns the previously multiplied data on the screen"""
+    def area(self):
+        return super().area()
