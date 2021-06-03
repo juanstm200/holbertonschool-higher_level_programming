7ffd36650197d8feb0635b7c274db51295493870
juanstm200 <juans.tovar70@gmail.com>
2021-06-02 20:54:47 -0500

    MyList

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a class MyList that inherits from list"""
+
+
+
+class MyList(list):
+    """prints the numerical data entered in an orderly manner"""
+    def print_sorted(self):
+        print(sorted(self))
