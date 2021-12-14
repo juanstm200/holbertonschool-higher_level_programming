2f6a4c8108b6121b9739a8081e9b89f1bd68bfed
juanstm200 <juans.tovar70@gmail.com>
2021-12-14 04:08:45 +0000

    multi_len - Javascript

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+//  but by using an array of string and a loop
+const langArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < langArray.length; i++) {
+  console.log(langArray[i]);
+}
