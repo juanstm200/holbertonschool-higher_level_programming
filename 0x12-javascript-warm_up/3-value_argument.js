ffc76847f9d2f7743f14631824589a924c08036c
juanstm200 <juans.tovar70@gmail.com>
2021-12-14 03:36:02 +0000

    value_argv_ - Javascript

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-if (process.argv[2])
-{
-    console.log(process.argv[2])
-}
-else 
-{
-    console.log("No argument")
+// script argv
+if (process.argv[2]) {
+  console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
