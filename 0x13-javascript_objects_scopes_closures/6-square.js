5497cfc00a5d50ee183ea765e6f7046a9a46878e
juanstm200 <juans.tovar70@gmail.com>
2021-12-14 23:01:28 -0500

    charPrint_square - Javascript

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const Square5 = require('./5-square.js');
+
+class Square extends Square5 {
+  charPrint (c = 'X') {
+    const character = c;
+    const { width, height } = this;
+    const shape = `${character.repeat(width)}\n`.repeat(height).slice(0, -1);
+    console.log(shape);
+  }
+}
+module.exports = Square;
