c3220079d878df287357002b56705031ac77ab5d
juanstm200 <juans.tovar70@gmail.com>
2021-12-19 01:57:23 -0500

    safe_filter - Python

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+'''
+akes in arguments and displays all values in the states table
+'''
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    database = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                                database=argv[3])
+    createeate = database.cursor()
+    createeate.execute("SELECT * from states\
+                WHERE name LIKE %s\
+                ORDER BY states.id", (argv[4],))
+    states = createeate.fetchall()
+    for state in states:
+        print(state)
+    createeate.close()
+    database.close()
