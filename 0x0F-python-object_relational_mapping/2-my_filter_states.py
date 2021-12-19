624205e4353674a6e0a8acd66298a81b216e2eb5
juanstm200 <juans.tovar70@gmail.com>
2021-12-19 01:46:11 -0500

    my_filter - python

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+'''
+Takes in an argument and displays all values in the states table
+'''
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    database = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                               database=argv[3])
+    create = database.cursor()
+    create.execute("SELECT * from states\
+                WHERE name LIKE '{}' COLLATE latin1_general_cs\
+                ORDER BY states.id".format(argv[4]))
+    states = create.fetchall()
+    for state in states:
+        print(state)
+    create.close()
+    database.close()
