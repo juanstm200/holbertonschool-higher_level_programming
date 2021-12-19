13de69d174b4f3266cae8b11fb6d1e1f7a50021e
juanstm200 <juans.tovar70@gmail.com>
2021-12-19 01:34:55 -0500

    select_create - Python

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,11 @@
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
-    db = MySQLdb.connect(user=argv[1],
-                         passwd=argv[2],
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2],
                          db=argv[3])
-    cr = db.cursor()
-    cr.execute("SELECT * from states ORDER BY states.id")
-    states = cr.fetchall()
+    create = db.cursor()
+    create.execute("SELECT * from states ORDER BY states.id")
+    states = create.fetchall()
     for state in states:
         print(state)
-    cr.close()
+    create.close()
