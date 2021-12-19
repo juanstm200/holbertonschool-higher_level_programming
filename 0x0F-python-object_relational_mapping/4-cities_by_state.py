5c23bd2caeb7bda6959e01ed69b3319c0dcabea7
juanstm200 <juans.tovar70@gmail.com>
2021-12-19 02:10:32 -0500

    cities_state - Python

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+''' Lists all cities from the database hbtn_0e_4_usa '''
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    datebase = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                               datebase=argv[3])
+    create = datebase.cursor()
+    create.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id")
+    cities = create.fetchall()
+    for city in cities:
+        print(city)
+    create.close()
+    datebase.close()
