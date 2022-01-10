537e8b97443cac8b9427ac4206e5686de1d26e4a
juanstm200 <juans.tovarm70@gmail.com>
2022-01-09 23:46:01 -0500

    statatus_display - python

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status """
+
+
+import requests
+
+
+if __name__ == "__main__":
+    solicitud = requests.get('https://intranet.hbtn.io/status')
+    text = solicitud.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
