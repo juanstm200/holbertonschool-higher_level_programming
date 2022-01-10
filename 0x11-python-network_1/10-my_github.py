23de2f24823c9b1aa94b9ac113c294cf2e6efe2f
juanstm200 <juans.tovarm70@gmail.com>
2022-01-09 23:58:25 -0500

    user_github - python

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" sends a POST request to http://0.0.0.0:5000/search_user """
+
+
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+
+if __name__ == '__main__':    
+    url = 'https://api.github.com/user'
+    rq = requests.get(url, auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(rq.json().get('id'))
