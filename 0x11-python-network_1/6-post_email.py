e9f169971a477c342a6afe52026a479c50ca94cf
juanstm200 <juans.tovarm70@gmail.com>
2022-01-09 23:53:44 -0500

    send_email - python

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" sends a POST request to the passed URL """
+
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    rq = requests.post(argv[1], data={'email': argv[2]})
+    print(rq.text)
