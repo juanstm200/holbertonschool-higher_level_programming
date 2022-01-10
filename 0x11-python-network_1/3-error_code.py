bc7a226c35e749cc66ad5c6bffed60790bf86e4f
juanstm200 <juans.tovarm70@gmail.com>
2022-01-09 23:42:58 -0500

    display_error - python

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" sends a request to the URL and displays the body of the response """
+
+
+import urllib.error as err
+import urllib.request as request
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = request.Request(argv[1])
+    try:
+        with request.urlopen(r) as rq:
+            print(rq.read().decode('utf-8'))
+    except err.HTTPError as e:
+        print("Error code: {}".format(e.code))
