01427922c61fbb12a7cd4640e2b809a4f355145e
juanstm200 <juans.tovarm70@gmail.com>
2022-01-09 23:55:39 -0500

    error_code - python

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" sends a request to the URL and displays the body of the response """
+
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    rq = requests.get(argv[1])
+    if rq.status_code >= 400:
+        print("Error code: {}".format(rq.status_code))
+    else:
+        print(rq.text)
