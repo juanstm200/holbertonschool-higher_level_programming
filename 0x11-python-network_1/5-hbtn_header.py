a8bbf004d07bddf67f0d135931edbf339634ebca
juanstm200 <juans.tovarm70@gmail.com>
2022-01-09 23:51:41 -0500

    header_ - python

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" """
+""" sends a request to the URL and displays the value """
 
 
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    res = requests.get(sys.argv[1])
-    print(res.headers.get('X-Request-Id'))
+    rq = requests.get(argv[1])
+    print(rq.headers.get('X-Request-Id'))
