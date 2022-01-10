d53fe90d2da1e791f29b69c2c4a0c8c46fa55c87
juanstm200 <juans.tovarm70@gmail.com>
2022-01-10 00:01:30 -0500

    json_ - python

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""letter and sends a POST request to http://0.0.0.0:5000/search_user """
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        letter = argv[1]
+    else:
+        letter = ""
+    rq = requests.post('http://0.0.0.0:5000/search_user', data={'q': letter})
+
+    try:
+        rq = res.json()
+        id = rq.get('id')
+        name = rq.get('name')
+        if len(rq) == 0 or not id or not name:
+            print("No result")
+        else:
+            print("[{}] {}".format(rq.get('id'), rq.get('name')))
+    except Exception:
+        print("Not a valid JSON")
