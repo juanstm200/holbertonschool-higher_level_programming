c1e4161b2466f206f3cb158dd9a0ea3a3d7a320f
juanstm200 <juans.tovarm70@gmail.com>
2022-01-09 23:31:45 -0500

    status - Python

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-""" script that fetches"""
+""" script fetches """
 
 
 import urllib.request
 
 
 if __name__ == "__main__":
-
-        with urllib.request.urlopen('https://intranet.hbtn.io/stat
-                                     us') as response:
-                html = response.read()
-                print("Body response:")
-                print("\t- type: {}".format(type(html)))
-                print("\t- content: {}".format(html))
-                print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    re_html = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(re_html) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
