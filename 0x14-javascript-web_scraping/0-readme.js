15691177ad27c744047746d0a53b7e74bc4e73b1
root <juans.tovar70@gmail.com>
2022-02-02 00:49:36 +0000

    readme_cisfun - Js_node

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 
-const fs = require('fs')
-const file_names = process.argv[2];
+const fs = require('fs');
+const FileName = process.argv[2];
 
-fs.readFile(file_names, 'utf8', function (err, line) {
-    if (err)
-    {
-        console.log(err);
-    }
-    else
-    {
-        console.log(line);
-    }
+fs.readFile(FileName, 'utf8', function (err, line) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(line);
+  }
 });
